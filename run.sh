1d640be3253a2148e02afcf7c0ebb0a790e466ae
HakkinDavid <desr11@hotmail.com>
2026-05-31 04:58:18 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 while true; do
     echo "===== STARTING CRAWLER ====="
 
-    PYTHONPATH=. ./venv/bin/python src/main.py --headless --harvest-only
+    PYTHONPATH=. ./venv/bin/python src/main.py --headless
 
     echo "===== CRAWLER FINISHED ====="
 
-    git add data/ export/ .gitattributes
+    git add data/ export/ tecate_reconstruction.blend *.glb .gitattributes
 
     git commit -m "Incremental Street View archival $(date)" || true
 
