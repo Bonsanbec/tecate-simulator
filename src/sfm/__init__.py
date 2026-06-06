4558341ec218331ba6ab23adb44591f74fd0aa7e
HakkinDavid <desr11@hotmail.com>
2026-06-05 20:16:30 -0700

    rm failed mvp

--- src/sfm/__init__.py
+++
@@ -1,8 +0,0 @@
-# Purpose: Declares the sfm package and exports camera model and colmap runner.
-# Inputs: None.
-# Outputs: None.
-# Responsibilities: Package declaration.
-# Dependencies: None
-
-from src.sfm.camera_model import yaw_to_quaternion, project_point, unproject_pixel
-from src.sfm.colmap_runner import ColmapRunner
